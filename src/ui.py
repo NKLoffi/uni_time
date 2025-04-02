266baf862b8f2e7e5363727c37adff80fc736541
NKLoffi <nklajiofficial@gmail.com>
2025-04-02 10:26:49 -0400

    setting up the base
    imported the required modules on each files

--- src/ui.py
+++ src/ui.py
@@ -1,2 +1,8 @@
 import sys
-from PyQt6 import (QtWidgets)
+from PyQt6.QtWidgets import (QPushbutton, QRadioButton, QLineEdit, QMainWindow, QWidget,
+                             QLabel, QMessageBox)
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super.__init__()
+        pass
