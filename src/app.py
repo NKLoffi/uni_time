533dae5c553ff06ad330c4ab5205cca0825dec76
NKLoffi <nklajiofficial@gmail.com>
2025-04-03 22:45:16 -0400

    MainWindow Set up
    setted up the main window and defined it's size

--- src/app.py
+++ src/app.py
@@ -1,7 +1,12 @@
 import sys
-from PyQt5.QtWidgets import QApplication
+from PyQt6.QtWidgets import QApplication
+from ui import MainWindow
 
 def main():
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec())
     pass
 
 if __name__ == "__main__":
--- src/ui.py
+++ src/ui.py
@@ -1,8 +1,18 @@
 import sys
-from PyQt6.QtWidgets import (QPushbutton, QRadioButton, QLineEdit, QMainWindow, QWidget,
+from PyQt6.QtWidgets import (QPushButton, QRadioButton, QLineEdit, QMainWindow, QWidget,
                              QLabel, QMessageBox)
 
 class MainWindow(QMainWindow):
     def __init__(self):
-        super.__init__()
+        super().__init__()
+        # self.db = Database()
+        # self.db.create_table()
+
+        self.setWindowTitle("Uni Time")
+        self.resize(800, 800)
+
+        self.initUI()
+        
+
+    def initUI(self):
         pass
