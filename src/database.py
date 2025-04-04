ea4ae29b86b7e87be3f64ee678491c5b0f10ab96
NKLoffi <nklajiofficial@gmail.com>
2025-04-04 07:58:53 -0400

    implemented database
    implemented the create table function and the function to insert task to the table.

--- src/database.py
+++ src/database.py
@@ -2,4 +2,39 @@ import sqlite3
 
 class Database:
     def __init__(self, db_name = "data.db"):
-        pass
+        self.db_name = db_name
+
+    def connect(self):
+        return sqlite3.connect(self.db_name)
+
+    def create_table(self): # function to create "task" table
+
+        CREATE_TASK_TABLE = """ CREATE TABLE IF NOT EXISTS tasks (
+                                taskId INTEGER AUTO INCREMENT PRIMARY KEY
+                                taskName TEXT NOT NULL
+                                courseName TEXT NOT NULL
+                                priority TEXT NOT NULL
+                                due DATE NOT NULL
+                                );"""
+        
+        connection = self.connect()
+        with connection:
+            connection.execute(CREATE_TASK_TABLE)
+        connection.close()
+
+
+    def insert_info(self, taskName, courseName, priority): # Function to insert tasks to the task table
+
+        INSERT_TASKS =  """ INSERT INTO tasks (
+                            taskName, 
+                            courseName,
+                            priority,
+                            due)
+                            values (?, ?, ?, ?);
+                            """
+        
+        connection = self.connect()
+        with connection:
+            connection.execute(INSERT_TASKS, (taskName, courseName, priority))
+        connection.close()
+            
