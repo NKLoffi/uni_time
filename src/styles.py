0c6d2fcaad45f1c0b8b668b86d686859eb8e297d
NKLoffi <nklajiofficial@gmail.com>
2025-04-21 13:01:37 -0400

    added more css styles
    Added more styling for push buttons and line edit

--- src/styles.py
+++ src/styles.py
@@ -23,7 +23,29 @@ WINDOW_STYLES = """
                 border-radius: 5px;
                 }
 
-                 QPushButton#started:hover{
-                 background-color: hsl(200, 72%, 35%);
-                 }
+                QPushButton#started:hover{
+                background-color: hsl(200, 72%, 35%);
+                }
+
+                QPushButton#cacc{
+                background-color: hsl(217, 67%, 44%);
+                color: white;
+                min-width: 150px;
+                max-width: 150px;
+                min-height: 25px;
+                max-height: 25px;
+                border-radius: 5px;
+                }
+
+                QLineEdit#caccLineEdit{
+                background-color: hsl(221, 13%, 28%);
+                border: solid black;
+                border-radius: 5px;
+                margin-left: 10px;
+                min-width: 200px;
+                max-width: 200px;
+                min-height: 25px;
+                max-height: 25px;
+                color: white;
+                }
                 """
