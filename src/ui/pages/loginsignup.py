08ba490b5c3363f294aabcbd3f479ea8e55650e1
NKLoffi <nklajiofficial@gmail.com>
2025-04-23 22:12:46 -0400

    added push buttons
    added buttons for log in  and sign in

--- src/ui/pages/loginsignup.py
+++ src/ui/pages/loginsignup.py
@@ -1,4 +1,4 @@
-from PyQt6.QtWidgets import QWidget, QVBoxLayout, QHBoxLayout, QLabel
+from PyQt6.QtWidgets import QWidget, QVBoxLayout, QHBoxLayout, QLabel, QPushButton
 from PyQt6.QtCore import Qt 
 
 def loginsignup(main_window):
@@ -12,8 +12,18 @@ def loginsignup(main_window):
     top_bar.addWidget(back_btn)
     top_bar.addStretch()
 
+    btn_layout = QVBoxLayout()
+    btn_layout.setSpacing(5)
+    login_btn = QPushButton("Login")
+    login_btn.setObjectName("l_btn")
+    signup_btn = QPushButton("Sign Up")
+    signup_btn.setObjectName("s_btn")
+
+    btn_layout.addWidget(login_btn, alignment=Qt.AlignmentFlag.AlignHCenter)
+    btn_layout.addWidget(signup_btn, alignment=Qt.AlignmentFlag.AlignHCenter)
+
+    layout.addLayout(btn_layout)
     layout.addLayout(top_bar)
 
     page.setLayout(layout)
     return page
-
