020db58ffea187afa9d4f5917f2d79636f8d6423
NKLoffi <nklajiofficial@gmail.com>
2025-04-27 22:43:18 -0400

    Errors and new Table
    fixed the errors and made a new table to store the users username and password

--- src/database.py
+++ src/database.py
@@ -7,10 +7,17 @@ class Database:
     def connect(self):
         return sqlite3.connect(self.db_name)
 
-    def create_table(self): # function to create "task" table
+    def create_table(self): # function to create table
+
+        CREATE_USER_TABLE = """ CREATE TABLE IF NOT EXISTS users (
+                                userId INTEGER PRIMARY KEY AUTOINCREMENT,
+                                userName TEXT NOT NULL,
+                                password TEXT NOT NULL
+                                );
+                            """
 
         CREATE_TASK_TABLE = """ CREATE TABLE IF NOT EXISTS tasks (
-                                taskId INTEGER AUTO INCREMENT PRIMARY KEY,
+                                taskId INTEGER PRIMARY KEY AUTOINCREMENT,
                                 taskName TEXT NOT NULL,
                                 courseName TEXT NOT NULL,
                                 priority TEXT NOT NULL,
@@ -20,10 +27,11 @@ class Database:
         connection = self.connect()
         with connection:
             connection.execute(CREATE_TASK_TABLE)
+            connection.execute(CREATE_USER_TABLE)
         connection.close()
 
 
-    def insert_info(self, taskName, courseName, priority): # Function to insert tasks to the task table
+    def insert_info(self, taskName, courseName, priority, due): # Function to insert tasks to the task table
 
         INSERT_TASKS =  """ INSERT INTO tasks (
                             taskName, 
@@ -35,6 +43,19 @@ class Database:
         
         connection = self.connect()
         with connection:
-            connection.execute(INSERT_TASKS, (taskName, courseName, priority))
+            connection.execute(INSERT_TASKS, (taskName, courseName, priority, due))
+        connection.close()
+
+    def create_user(self, userName, password):
+
+        INSERT_USER = """ INSERT INTO users (
+                            userName,
+                            password)
+                            values(?, ?);
+                            """
+        
+        connection = self.connect()
+        with connection:
+            connection.execute(INSERT_USER, (userName, password))
         connection.close()
             
