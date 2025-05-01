6bfaa12fe2386d9fff24003f29a2e04092fe232d
NKLoffi <nklajiofficial@gmail.com>
2025-05-01 13:53:49 -0400

    Styiling
    Added styling for buttons and lineEdits

--- src/styles.py
+++ src/styles.py
@@ -4,11 +4,22 @@ WINDOW_STYLES = """
                 background-color: hsl(40, 3%, 28%);
                 }
 
-                QPushButton#settingsButton{
-                background-color: transparent; 
-                border: none;
+                QLabel#mainLabel,
+                QLabel#label_3{
+                color: white;
+                font-weight: bold;
+                font-size: 30px;
+                text-align: center;
                 }
-                QPushButton#back{
+
+                QLabel#label_4{
+                color: white;
+                font-weight: bold;
+                font-size: 23px;
+                text-align: center;
+                }
+
+                QPushButton#backButton{
                 background-color: transparent;
                 border: none;
                 }
@@ -47,7 +58,9 @@ WINDOW_STYLES = """
                 border-radius: 5px;
                 }
 
-                QPushButton#s_btn{
+                QPushButton#createAccButton,
+                QPushButton#pushButton,
+                QPushButton#pushButton_2{
                 background-color: hsl(217, 67%, 44%);
                 color: white;
                 min-width: 150px;
@@ -57,13 +70,13 @@ WINDOW_STYLES = """
                 border-radius: 5px;
                 }
 
-                QLineEdit#caccLineEdit{
-                background-color: hsl(221, 13%, 28%);
+                QLineEdit{
+                background-color: hsl(40, 3%, 37%);
                 border: solid black;
                 border-radius: 5px;
                 margin-left: 10px;
-                min-width: 200px;
-                max-width: 200px;
+                min-width: 300px;
+                max-width: 400px;
                 min-height: 25px;
                 max-height: 25px;
                 color: white;
